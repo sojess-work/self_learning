43f9a25d82d9381d23012fe53bd001aad69ec113
sojess-work <92088687+sojess-work@users.noreply.github.com>
2021-10-08 11:34:33 +0530

    Update Candies.java

--- java/functions/Candies.java
+++ java/functions/Candies.java
@@ -1,30 +1,24 @@
-package functions;
-
 import java.util.ArrayList;
-
-public class Candies {
-    public static void main(String[] args) {
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         ArrayList<Boolean> list = new ArrayList<>();
-        int[] candies = {2,3,5,1,3};
-        int extraCandies=3;
-        int maxCandies = maxCandies(candies);
-        for(int num : candies){
-            if(num+extraCandies>maxCandies){
-                list.add(true);
-            }else{
-                list.add(false);
-            }
-        }
-        System.out.println(list);
-
-    }
-    public static int maxCandies(int[] candies){
         int maxCandies=0;
+       
+        //finding the greatest amount of candies among kids 
         for(int num :candies){
             if(num>maxCandies){
                 maxCandies=num;
             }
         }
-        return maxCandies;
+        //adding the extra candies among each kids and finding if someone have more candies than maxCandies
+        for(int num : candies){
+            if(num+extraCandies>=maxCandies){
+                list.add(true);
+            }else{
+                list.add(false);
+            }
+        }
+      return list;  
     }
+}
 }
